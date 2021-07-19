9e77fbdc7721ea459847faa659f6c79c5bc99d5b
Melanie <meeelu.monti17@hotmail.com>
2021-07-19 19:51:41 -0300

    Fix nombre instancia mongodb

--- entregable_17/src/config/database.js
+++ entregable_17/src/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb://172.27.31.170:27017/ecommerce',
+    url: 'mongodb://localhost:27017/ecommerce',
     options: {
         useNewUrlParser: true,
         useUnifiedTopology: true
